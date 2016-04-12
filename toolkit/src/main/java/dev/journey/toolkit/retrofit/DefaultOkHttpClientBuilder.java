db3cc38b4503495d5193bd76a67c81a564b927d2
孟卫平 <mengweiping@qufenqi.com>
2016-04-12 10:38:53 +0800

    添加容错

--- toolkit/src/main/java/dev/journey/toolkit/retrofit/DefaultOkHttpClientBuilder.java
+++ toolkit/src/main/java/dev/journey/toolkit/retrofit/DefaultOkHttpClientBuilder.java
@@ -6,6 +6,7 @@ import android.webkit.CookieManager;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import dev.journey.toolkit.util.StdFileUtils;
 import okhttp3.Cache;
@@ -23,6 +24,9 @@ public class DefaultOkHttpClientBuilder {
     public static OkHttpClient.Builder newInstance(Context context) {
         Cache cache = new Cache(StdFileUtils.getDiskCacheDir(context, "okhttp"), DEFAULT_CACHE_SIZE);
         OkHttpClient.Builder builder = new OkHttpClient.Builder()
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS)
                 .cache(cache)
                 .cookieJar(new CookieJar() {
                     @Override
@@ -35,12 +39,16 @@ public class DefaultOkHttpClientBuilder {
                     @Override
                     public List<Cookie> loadForRequest(HttpUrl httpUrl) {
                         List<Cookie> cookies = new ArrayList<Cookie>();
-                        URL url = httpUrl.url();
-                        String domain = CookieUtils.getDomain(url);
-                        if (domain != null) {
-                            String cookieOfWebView = CookieManager.getInstance().getCookie(domain);
-                            List<Cookie> cookieListOfWebView = CookieUtils.parseCookieList(httpUrl, cookieOfWebView);
-                            cookies = CookieUtils.mergeCookie(cookies, cookieListOfWebView);
+                        try {
+                            URL url = httpUrl.url();
+                            String domain = CookieUtils.getDomain(url);
+                            if (domain != null) {
+                                String cookieOfWebView = CookieManager.getInstance().getCookie(domain);
+                                List<Cookie> cookieListOfWebView = CookieUtils.parseCookieList(httpUrl, cookieOfWebView);
+                                cookies = CookieUtils.mergeCookie(cookies, cookieListOfWebView);
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
                         }
                         return cookies;
                     }
