4e2ca9246b50c0a1640b58c3318f33793811c545
孟卫平 <mengweiping@qufenqi.com>
2016-04-13 19:25:10 +0800

    add 判断忽略升级方法

--- toolkit/src/main/java/dev/journey/toolkit/util/VersionUtils.java
+++ toolkit/src/main/java/dev/journey/toolkit/util/VersionUtils.java
@@ -1,14 +1,21 @@
 package dev.journey.toolkit.util;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.text.TextUtils;
 
 /**
  * Created by mwp on 16/4/13.
  */
 public class VersionUtils {
+
+    public static final String KEY_UPG_SPF = "KEY_UPG_SPF";
+    public static final String KEY_LAST_IGNORED_VERSION = "KEY_LAST_IGNORED_VERSION";
+
     /**
      * 比较两个版本号的大小
      * 如果version1>version2，返回1，小于则返回-1，相等则返回0
@@ -61,5 +68,59 @@ public class VersionUtils {
             e.printStackTrace();
         }
         return versionName;
+    }
+
+    public static void saveIgnoredVersion(Context context, String targetVersion) {
+        if (!TextUtils.isEmpty(targetVersion)) {
+            try {
+                Uri uri = new Uri.Builder()
+                        .scheme("toolkit")
+                        .authority("dev.journey.toolkit.util.saveIgnoredVersion")
+                        .appendQueryParameter("version", targetVersion)
+                        .appendQueryParameter("time", Long.toString(System.currentTimeMillis()))
+                        .build();
+                SharedPreferences sp = context.getSharedPreferences(KEY_UPG_SPF, Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putString(KEY_LAST_IGNORED_VERSION, uri.toString()).commit();
+            } catch (Exception e) {
+                L.e("VersionUtils saveIgnoredVersion", e);
+            }
+        }
+    }
+
+    /**
+     * 检查忽略升级是否失效，失效则重新提示升级
+     *
+     * @param targetVersion
+     * @return
+     */
+    public static boolean isVersionIgnoreExpired(Context context, String targetVersion, long validateInterval) {
+        SharedPreferences sp = context.getSharedPreferences(KEY_UPG_SPF, Context.MODE_PRIVATE);
+        String ignoreSchemer = sp.getString(KEY_LAST_IGNORED_VERSION, null);
+        if (!TextUtils.isEmpty(ignoreSchemer)) {
+            try {
+                Uri uri = Uri.parse(ignoreSchemer);
+                if (uri != null) {
+                    String version = uri.getQueryParameter("version");
+                    String time = uri.getQueryParameter("time");
+                    if (version != null && version.equals(targetVersion) && time != null) {
+                        try {
+                            long interval = System.currentTimeMillis() - Long.parseLong(time);
+                            boolean expired = interval > validateInterval;
+                            L.d("VersionUtils isIgnoreExpired", "interval=" + interval
+                                    + "\nvalidateInterval=" + validateInterval
+                                    + "\nexpired=" + expired);
+                            return expired;
+                        } catch (Exception e) {
+                            L.e("VersionUtils isIgnoreExpired", e);
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                L.e("VersionUtils isIgnoreExpired", e);
+            }
+
+        }
+        return true;
     }
 }
