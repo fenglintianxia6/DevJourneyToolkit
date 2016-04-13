1935e0a8960082f019484f3a2ed6476949f96342
孟卫平 <mengweiping@qufenqi.com>
2016-04-13 14:21:40 +0800

    添加静态方法

--- toolkit/src/main/java/dev/journey/toolkit/config/ApiConfig.java
+++ toolkit/src/main/java/dev/journey/toolkit/config/ApiConfig.java
@@ -1,11 +1,16 @@
 package dev.journey.toolkit.config;
 
 import dev.journey.toolkit.util.HttpUtils;
+import okhttp3.MediaType;
 
 /**
  * Created by mwp on 2016/1/22.
  */
 public class ApiConfig {
+
     public static final String KEY_USER_AGENT = "User-Agent";
     public static final String DEFAULT_USER_AGENT = HttpUtils.defaultUserAgent();
+
+    public static MediaType MEDIA_TYPE_OCTET_STREAM = MediaType.parse("application/octet-stream");
+    public static MediaType MEDIA_TYPE_TEXT_PLAIN = MediaType.parse("text/plain; charset=UTF-8");
 }
--- toolkit/src/main/java/dev/journey/toolkit/util/StdFileUtils.java
+++ toolkit/src/main/java/dev/journey/toolkit/util/StdFileUtils.java
@@ -4,8 +4,12 @@ import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
 import android.os.Environment;
+import android.text.TextUtils;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class StdFileUtils {
 
@@ -93,5 +97,44 @@ public class StdFileUtils {
             L.d("StdFileUtils", "getSdCardDir: dir=" + dir.getAbsolutePath());
             return dir;
         }
+    }
+
+    /**
+     * 根据byte数组，生成文件
+     */
+    public static boolean bytesToFile(byte[] bfile, File file) {
+        BufferedOutputStream bos = null;
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(file);
+            bos = new BufferedOutputStream(fos);
+            bos.write(bfile);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (bos != null) {
+                try {
+                    bos.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
+        return false;
+    }
+
+    public static String getFileNameFromUrl(String url) {
+        if (TextUtils.isEmpty(url) || !url.contains("/")) {
+            return url;
+        }
+        return url.substring(url.lastIndexOf("/"), url.length());
     }
 }
