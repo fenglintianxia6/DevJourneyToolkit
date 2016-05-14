2b8312d2b9c277c26843a1eac0ff164d392c10bf
孟卫平 <mengweiping@qufenqi.com>
2016-05-14 17:05:09 +0800

    add acra

---
+++ apptoolkit/src/main/java/com/qufenqi/android/apptoolkit/acra/CrashLocalSender.java
@@ -0,0 +1,47 @@
+package com.qufenqi.android.apptoolkit.acra;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import org.acra.collector.CrashReportData;
+import org.acra.config.ACRAConfiguration;
+import org.acra.sender.ReportSender;
+import org.acra.sender.ReportSenderException;
+
+import java.io.File;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import dev.journey.toolkit.util.L;
+import dev.journey.toolkit.util.StdFileUtils;
+import dev.journey.toolkit.util.TimeUtils;
+
+/**
+ * Created by mengweiping on 16/5/14.
+ */
+public class CrashLocalSender implements ReportSender {
+    ACRAConfiguration config;
+    ExecutorService executorService;
+
+    public CrashLocalSender(ACRAConfiguration config) {
+        this.config = config;
+        executorService = Executors.newSingleThreadExecutor();
+    }
+
+    @Override
+    public void send(@NonNull final Context context, @NonNull final CrashReportData errorContent) throws ReportSenderException {
+        L.d("CrashLocalSender", errorContent == null ? null : errorContent.toString());
+        if (errorContent == null) {
+            return;
+        }
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                File dir = StdFileUtils.getDiskCacheDir(context, "Crash");
+                File file = new File(dir, TimeUtils.createTimeStamp() + ".txt");
+                String crashInfo = errorContent.toString();
+                StdFileUtils.bytesToFile(crashInfo.getBytes(), file);
+            }
+        });
+    }
+}
---
+++ apptoolkit/src/main/java/com/qufenqi/android/apptoolkit/acra/CrashSenderFactory.java
@@ -0,0 +1,19 @@
+package com.qufenqi.android.apptoolkit.acra;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import org.acra.config.ACRAConfiguration;
+import org.acra.sender.ReportSender;
+import org.acra.sender.ReportSenderFactory;
+
+/**
+ * Created by mengweiping on 16/5/14.
+ */
+public class CrashSenderFactory implements ReportSenderFactory {
+    @NonNull
+    @Override
+    public ReportSender create(@NonNull Context context, @NonNull ACRAConfiguration config) {
+        return new CrashLocalSender(config);
+    }
+}
