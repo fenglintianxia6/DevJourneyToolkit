d59f4fd1abaeecea1e8166133f66a526a8ac6ec9
Mwp <mwping1324@163.com>
2016-05-15 22:42:06 +0800

    fix sample

--- app/src/main/java/dev/journey/toolkitapp/MainActivity.java
+++ app/src/main/java/dev/journey/toolkitapp/MainActivity.java
@@ -1,39 +1,15 @@
 package dev.journey.toolkitapp;
 
-import android.app.ProgressDialog;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.widget.TextView;
-
-import java.io.File;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import dev.journey.toolkit.faulttolerant.DialogUtils;
-import dev.journey.toolkit.faulttolerant.ToastUtils;
-import dev.journey.toolkit.task.FileDirDeleteTask;
-import dev.journey.toolkit.task.FileDirLengthCalculateTask;
-import dev.journey.toolkit.util.L;
-import dev.journey.toolkit.util.StdFileUtils;
-import dev.journey.toolkit.util.StringUtils;
-import dev.journey.uitoolkit.view.FlexibleTabLayout;
 
 public class MainActivity extends AppCompatActivity {
-    float density;
-    ProgressDialog progressDialog;
     @BindView(R.id.toolbar)
     Toolbar toolbar;
-    @BindView(R.id.textView)
-    TextView textView;
-    @BindView(R.id.flexibleTabLayout)
-    FlexibleTabLayout flexibleTabLayout;
-    @BindView(R.id.tabLayout)
-    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,92 +18,5 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         setSupportActionBar(toolbar);
-
-        density = getResources().getDisplayMetrics().density;
-        progressDialog = new ProgressDialog(this);
-        progressDialog.setCancelable(false);
-
-        File dir = StdFileUtils.getSdCardDir(MainActivity.this, "QuFenQiBD", false);
-        calculateCacheSize(dir);
-
-        tabLayout.addTab(tabLayout.newTab().setText("1").setIcon(getResources().getDrawable(R.mipmap.ic_launcher)));
-        tabLayout.addTab(tabLayout.newTab().setText("1").setIcon(getResources().getDrawable(R.mipmap.ic_launcher)));
-        tabLayout.addTab(tabLayout.newTab().setText("1").setIcon(getResources().getDrawable(R.mipmap.ic_launcher)));
-        tabLayout.addTab(tabLayout.newTab().setText("1").setIcon(getResources().getDrawable(R.mipmap.ic_launcher)));
-
-        flexibleTabLayout.addTab(flexibleTabLayout.newTab().setText("1").setIcon(getResources().getDrawable(R.mipmap.ic_launcher)));
-        flexibleTabLayout.addTab(flexibleTabLayout.newTab().setText("1").setIcon(getResources().getDrawable(R.mipmap.ic_launcher)));
-        flexibleTabLayout.addTab(flexibleTabLayout.newTab().setText("1").setIcon(getResources().getDrawable(R.mipmap.ic_launcher)));
-        flexibleTabLayout.addTab(flexibleTabLayout.newTab().setText("1").setIcon(getResources().getDrawable(R.mipmap.ic_launcher)));
-    }
-
-    private void calculateCacheSize(File dir) {
-        FileDirLengthCalculateTask task = new FileDirLengthCalculateTask(this, new FileDirLengthCalculateTask.FileDirLengthCalculateListener() {
-            @Override
-            public void onStart() {
-                textView.setText("正在计算缓存大小");
-            }
-
-            @Override
-            public void onFailure(Throwable throwable) {
-                textView.setText("获取缓存大小失败！");
-            }
-
-            @Override
-            public void onSuccess(long length) {
-                textView.setText(StringUtils.formatDouble(StdFileUtils.toMB(length)) + "MB");
-            }
-        });
-        task.addFileOrDir(dir);
-        task.checkAndStart();
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_clear_cache: {
-                File dir = StdFileUtils.getSdCardDir(MainActivity.this, "QuFenQiBD");
-                clearCache(dir);
-                long length = StdFileUtils.getFileDirectorySize(dir);
-                L.d("MainActivity", "after deleteFile, dir length = " + StringUtils.formatDouble(StdFileUtils.toMB(length)) + "MB");
-                return true;
-            }
-            default: {
-                return super.onOptionsItemSelected(item);
-            }
-        }
-    }
-
-    private void clearCache(File dir) {
-        FileDirDeleteTask dirDeleteTask = new FileDirDeleteTask(this, new FileDirDeleteTask.IFileDirDeleteListener() {
-            @Override
-            public void onStart() {
-                progressDialog.setMessage("正在清理缓存，请稍候...");
-                DialogUtils.showDialog(MainActivity.this, progressDialog);
-            }
-
-            @Override
-            public void onFailure(Throwable throwable) {
-                DialogUtils.dismissDialog(MainActivity.this, progressDialog);
-                ToastUtils.showToast(MainActivity.this, throwable.getMessage());
-            }
-
-            @Override
-            public void onSuccess() {
-                DialogUtils.dismissDialog(MainActivity.this, progressDialog);
-                ToastUtils.showToast(MainActivity.this, "清理成功");
-                File dir = StdFileUtils.getSdCardDir(MainActivity.this, "QuFenQiBD", false);
-                long length = StdFileUtils.getFileDirectorySize(dir);
-                textView.setText(StringUtils.formatDouble(StdFileUtils.toMB(length)) + "MB");
-            }
-        }).addFileOrDir(dir);
-        dirDeleteTask.checkAndStart();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main, menu);
-        return true;
     }
 }
---
+++ app/src/main/java/dev/journey/toolkitapp/SampleBean.java
@@ -0,0 +1,45 @@
+package dev.journey.toolkitapp;
+
+
+import java.util.List;
+
+public class SampleBean {
+
+    private DataBean data;
+
+    public DataBean getData() {
+        return data;
+    }
+
+    public void setData(DataBean data) {
+        this.data = data;
+    }
+
+    public static class DataBean {
+        /**
+         * name : Zhang San
+         */
+
+        private List<ListBean> list;
+
+        public List<ListBean> getList() {
+            return list;
+        }
+
+        public void setList(List<ListBean> list) {
+            this.list = list;
+        }
+
+        public static class ListBean {
+            private String name;
+
+            public String getName() {
+                return name;
+            }
+
+            public void setName(String name) {
+                this.name = name;
+            }
+        }
+    }
+}
